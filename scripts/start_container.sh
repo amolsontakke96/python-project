0eabaa62023cbd34acfe4579965791bb2dc2d2c1
amolsontakke96 <amolsontakke96@gmail.com>
2025-07-17 08:12:21 +0000

    file releated to code deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+set -e
+
+# pull docker image from docker hub
+
+docker pull amolsontakke96/sample-python:latest
+
+# Run docker image as a container
+
+docker run -d -p 5000:5000 amolsontakke96/sample-python:latest
