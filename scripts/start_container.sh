e13238591444ee1774d7fa3d2bf9160b74fa27e6
AmolSontakke <118731985+amolsontakke96@users.noreply.github.com>
2025-07-17 15:22:50 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,10 +1,8 @@
-#!bin/bash
+#!/bin/bash
 set -e
-set -x
-# pull docker image from docker hub
 
+# pull docker image from docker hub
 docker pull amolsontakke96/sample-python
 
 # Run docker image as a container
-
 docker run -d -p 5000:5000 amolsontakke96/sample-python
